dde1d8174cc25555b8bc4f1f123f67351b478100
ethanstarchere <40360206+ethanstarchere@users.noreply.github.com>
2018-06-18 11:28:50 -0400

    Initial Solution Setup

---
+++ MyActualShop/MyActualShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyActualShop.WebUI.MvcApplication" Language="C#" %>
---
+++ MyActualShop/MyActualShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyActualShop.WebUI.Startup))]
+namespace MyActualShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
